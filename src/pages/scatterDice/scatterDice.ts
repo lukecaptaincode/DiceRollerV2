4a2fd93578270d3b0306582d605e5d71e4d1bdb5
Luke Captain <lukecaptain@hotmail.com>
2018-07-04 08:11:10 +0100

    added adsense

--- src/pages/scatterDice/scatterDice.ts
+++ src/pages/scatterDice/scatterDice.ts
@@ -1,9 +1,5 @@
-import {
-  Component
-} from '@angular/core';
-import {
-  Random
-} from '../home/home';
+import { Component } from '@angular/core';
+import { Random } from '../home/home';
 @Component({
   selector: 'page-scatterDice',
   templateUrl: 'scatterDice.html'
@@ -14,14 +10,19 @@ export class ScatterDicePage {
   random: any;
   deg: any;
   degDec: any;
+  scatter_distance: any;
 
   scatter() {
-    var scatterDiceContainer = document.getElementById("scatterDiceContainer");
-    var scatter = document.getElementById("scatter");
-    this.d = new Date();//declare new date for random seeding
-    this.n = this.d.getMilliseconds();//get the date seed to milliseconds
-    this.random = new Random(this.n);//declare new random seeding with current date time to milliseconds
-    if(this.random.nextInt32([0,360])%60 == 0||this.random.nextInt32([0,360])%30 == 0){
+    var scatterDiceContainer = document.getElementById('scatterDiceContainer');
+    var scatter = document.getElementById('scatter');
+    var distance_amount = document.getElementById('scatterDistance');
+    this.d = new Date(); //declare new date for random seeding
+    this.n = this.d.getMilliseconds(); //get the date seed to milliseconds
+    this.random = new Random(this.n); //declare new random seeding with current date time to milliseconds
+    if (
+      this.random.nextInt32([0, 360]) % 60 == 0 ||
+      this.random.nextInt32([0, 360]) % 30 == 0
+    ) {
       scatter.innerHTML = 'X';
       scatterDiceContainer.style.webkitTransform = 'rotate(0deg)';
       scatterDiceContainer.style.transform = 'rotate(0deg)';
@@ -29,11 +30,15 @@ export class ScatterDicePage {
       return;
     }
     scatter.classList.remove('hit');
-    this.deg = this.random.nextInt32([0,360]);//random to result to ensure dice and temp have same values
+    this.deg = this.random.nextInt32([0, 360]); //random to result to ensure dice and temp have same values
     this.degDec = this.random.nextInt32();
     scatter.innerHTML = '&uarr;';
-    scatterDiceContainer.style.webkitTransform = 'rotate(' + this.deg+"."+this.degDec+ 'deg)';
-    scatterDiceContainer.style.transform = 'rotate(' + this.deg+"."+this.degDec+ 'deg)';
+    distance_amount.innerHTML =
+      'Scatter by: ' + this.random.nextInt32([2, 12]) + ' inches';
+    scatterDiceContainer.style.webkitTransform =
+      'rotate(' + this.deg + '.' + this.degDec + 'deg)';
+    scatterDiceContainer.style.transform =
+      'rotate(' + this.deg + '.' + this.degDec + 'deg)';
     scatterDiceContainer.style.transformOrigin = '50% 50%';
     scatterDiceContainer.style.webkitTransformOrigin = '50% 50%';
   }
